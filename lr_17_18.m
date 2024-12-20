f0eddb6a9263c5ee709e1d88e44126ca07ffc814
efftea <131270184+efftea@users.noreply.github.com>
2024-12-20 03:11:56 +0300

    Выполнено 19-20

--- lr_17_18.m
+++
@@ -1,8 +0,0 @@
-x1 = linspace(-1, 1, 10)
-x2 = linspace(-1, 1, 10)
-
-[X1, X2] = meshgrid(x1, x2);
-
-Y = 2 * (X1 - 1).^2 + (1 + X2.^2)
-
-dlmwrite('testing_data.dat', [X1(:), X2(:), Y(:)], 'delimiter', '\t');
